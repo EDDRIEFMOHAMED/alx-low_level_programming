d29bc3724016dfada8499bbf79dcc46d14e73656
eddriefmohamed <mohamed.ed.drief@gmail.com>
2023-03-21 17:00:23 -0700

    0putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,7 +3,7 @@
  *
  */
 
- #include <unistd.h> 
+ #include "main.h" 
 
  /** 
  * main-print putchar followed by a new line
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+
+/* Prototype de la fonction _putchar */
+int _putchar(char c);
+
+#endif /* MAIN_H */
